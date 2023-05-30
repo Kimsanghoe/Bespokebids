3a63fbeaf4985f75015c07d892776c40b6f5b5db
류성현 <simon0823@naver.com>
2023-05-30 16:01:00 +0900

    react spring 연결

---
+++ BespokeBids/src/main/java/Bespoke/BespokeBids/controller/MemberController.java
@@ -0,0 +1,39 @@
+package Bespoke.BespokeBids.controller;
+
+import Bespoke.BespokeBids.LoginType;
+import Bespoke.BespokeBids.MemberStatus;
+import Bespoke.BespokeBids.MemberType;
+import Bespoke.BespokeBids.domain.Member;
+import Bespoke.BespokeBids.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("user")
+public class MemberController {
+
+    private final MemberService memberService;
+
+
+
+    @PostMapping(value = "/join_our")
+    public String join(@RequestParam(value = "user_id") String userId,
+                       @RequestParam(value = "password") String password,
+                       @RequestParam(value = "email") String email,
+                       @RequestParam(value = "phone_number") String phoneNumber,
+                       @RequestParam(value = "address") String address,
+                       @RequestParam(value = "user_name") String userName
+                       ) {
+        Member member = new Member(userId, password, email, phoneNumber, address, userName, LocalDateTime.now(), MemberStatus.ACTIVE, MemberType.BUYER, LoginType.OUR);
+        memberService.join(member);
+        System.out.println("실행됨");
+        return "redirect:/";
+    }
+
+}
---
+++ BespokeBids/src/main/java/Bespoke/BespokeBids/service/MemberService.java
@@ -0,0 +1,51 @@
+package Bespoke.BespokeBids.service;
+
+import Bespoke.BespokeBids.domain.Member;
+import Bespoke.BespokeBids.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    /**
+     *  회원 가입
+     */
+    @Transactional
+    public UUID join(Member member) {
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    /**
+     *  중복 회원 검증 로직
+     * */
+    private void validateDuplicateMember(Member member) {
+        //EXCEPTION
+        List<Member> findMembers = memberRepository.findByUserId(member.getUserId());
+        if (!findMembers.isEmpty()) {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
+    }
+    /**
+     *
+     */
+
+    public List<Member> findMember() {
+        return memberRepository.findAll();
+    }
+
+    public Member findOne(UUID memberId) {
+        return memberRepository.findOne(memberId);
+    }
+
+}
